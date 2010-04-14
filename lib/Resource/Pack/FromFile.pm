9520ea84a867da73c49cf9df9c68eacc1ee69290
Jesse Luehrs <doy@tozt.net>
2010-04-14 17:45:21 -0500

    add a way to define resource files for resource definitions

---
+++ lib/Resource/Pack/FromFile.pm
@@ -0,0 +1,26 @@
+package Resource::Pack::FromFile;
+use Moose;
+use MooseX::Types::Path::Class qw(File);
+use Resource::Pack;
+
+extends 'Resource::Pack::Resource';
+
+has resource_file => (
+    is       => 'ro',
+    isa      => File,
+    coerce   => 1,
+    required => 1,
+);
+
+sub BUILD {
+    my $self = shift;
+    resource $self => as {
+        install_from(Path::Class::File->new($self->resource_file)->parent);
+        include($self->resource_file);
+    };
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
